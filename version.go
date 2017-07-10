0cadfa79db6847e69fc8f15c9153d0988f6fa59d
luis <luisf@hexasoftware.com>
2017-07-10 17:25:29 -0300

    version change

--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package main
 
 const (
   //Version contains version of the package
-  Version = "0.2-2-g4eba960 - built: 2017-07-10 20:23:51 UTC"
+  Version = "0.3 - built: 2017-07-10 20:25:20 UTC"
 )
