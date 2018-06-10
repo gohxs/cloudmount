4f8c91287f0ff7f6d4e90ac5a377a761f1dbb857
luis <luisf@hexasoftware.com>
2018-06-10 10:43:12 -0300

    bumping version, fixing #4

--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package main
 
 const (
   //Version contains version of the package
-  Version = "0.7-7-g154f589 - built: 2018-06-10 13:40:59 UTC"
+  Version = "0.7-9-g359da9e - built: 2018-06-10 13:43:05 UTC"
 )
